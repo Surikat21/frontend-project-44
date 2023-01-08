483ce5f20dd09ae046191acd551edb6999bd1aa5
Surikat21 <oparin.s.dkb2@mail.ru>
2023-01-08 21:12:31 +0700

    update

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+export default function greetings (userName) {
+console.log ('Welcome to the Brain Games!')
+//var readlineSync = require('readline-sync');
+const userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+}
