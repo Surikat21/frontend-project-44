54d90b25d9fd78bbb2691616a651ce09536a805e
Surikat21 <oparin.s.dkb2@mail.ru>
2023-01-09 21:39:12 +0700

    greetings userName

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
-export default function greetings (userName) {
+
+export default function greetings () {
 console.log ('Welcome to the Brain Games!')
-//var readlineSync = require('readline-sync');
+var readlineSync = require('readline-sync');
+// Wait for user's response.
 const userName = readlineSync.question('May I have your name? ');
 console.log('Hi ' + userName + '!');
 }
