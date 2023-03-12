520089650ab31e9a5811a900487edbc2ec3d20c3
Surikat21 <oparin.s.dkb2@mail.ru>
2023-03-12 20:15:14 +0700

    update progression

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -4,10 +4,11 @@ import getGame from '../index.js';
 const taskGame = 'What number is missing in the progression?';
 
 function startGame() {
+  const length = getRandomNumber(5, 10)
   const result = [];
   const number1 = getRandomNumber(1, 10); // ищем рандомное число
   const number2 = getRandomNumber(1, 10); // ищем рандомное число
-  for (let a = 0; a < 9; a += 1) {
+  for (let a = 0; a < length; a += 1) {
     result[0] = number1;
     result[a + 1] = result[a] + number2;
     result.push(result);
