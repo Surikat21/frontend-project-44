008127aac6b756faee06b4ef453a46d66f2212d4
Surikat21 <oparin.s.dkb2@mail.ru>
2023-03-12 19:40:24 +0700

    update Welcome

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,6 @@ export default function greetings() {
   console.log('Welcome to the Brain Games!');
   // Wait for user's response.
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  console.log(`Hello, ${userName}`);
   return userName;
 }
