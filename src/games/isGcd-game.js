42e9bc23bab1706ca5d65b76249288aba7beb729
Surikat21 <oparin.s.dkb2@mail.ru>
2023-03-14 17:55:09 +0700

    update nod

--- src/games/isGcd-game.js
+++ src/games/isGcd-game.js
@@ -3,14 +3,12 @@ import getGame from '../index.js';
 
 const taskGame = 'Find the greatest common divisor of given numbers.';
 
-function nod(number1, number2) { // вычисление НОД
-  while (number2) {
-    const k = number2;
-    number2 = number1 % number2;
-    number1 = k;
+const nod = (number1, number2) => {
+  if (number2 === 0) {
+    return number1;
   }
-  return number1;
-}
+  return nod(number2, number1 % number2);
+};
 
 function startGame() {
   const number1 = getRandomNumber(1, 10); // ищем рандомное число
